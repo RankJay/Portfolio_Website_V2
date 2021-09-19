648c14dcd52719ba6b499b2315aae9bb023df1ad
RankJay <rankjay0101@gmail.com>
2021-09-19 10:32:40 +0530

    bug fix for day 'Sunday'

--- src/js/index.js
+++ src/js/index.js
@@ -34,7 +34,7 @@ window.onload = function() {
     //   }
     // }
 
-    var dayMapper = {"1": "Monday", "2": "Tuesday", "3":"Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday", "7": "Sunday"}
+    var dayMapper = {"0": "Sunday", "1": "Monday", "2": "Tuesday", "3":"Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday"}
     var MonthMapper = {"1": "January", "2": "February", "3": "March", "4": "April", "5":"May", "6": "June", "7": "July", "8": "August", "9": "September", "10": "October", "11": "November", "12": "December"};
     var today = new Date();
     var timeInHour = today.getHours();
