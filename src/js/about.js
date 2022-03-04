beaaef70d137870a04cc5c8d7d03cc547b578fcf
RankJay <rankjay0101@gmail.com>
2022-03-04 13:34:39 +0530

    adding aboutMe Page

--- src/js/about.js
+++ src/js/about.js
@@ -1,58 +1,91 @@
 window.onload = function() {
 
-    var pageTitle = document.title;
-    var attentionMessage1 = 'Awee...Come Back!';
-    var attentionMessage2 = 'Psssss....';
-    var blinkEvent = null;
-    var today = new Date();
-    var timeInHour = today.getHours();
-    console.log(timeInHour)
+  var pageTitle = document.title;
+  var attentionMessage1 = 'Awee...Come Back!';
+  var attentionMessage2 = 'Psssss....';
+  var blinkEvent = null;
 
-    if (timeInHour >= 19 || timeInHour < 5) {
-        var bodyNightTheme = document.getElementById('bodyNightTheme');
-        var SVGNightTheme = document.getElementById('SVGNightTheme');
-        var HeadingNightTheme = document.getElementById('HeadingNightTheme');
-        var DescriptionNightTheme = document.getElementById('DescriptionNightTheme');
-        var Cookie = document.getElementById('Cookie');
-        var CookieHeading = document.getElementById('CookieHeading');
-        var CookieDes = document.getElementById('CookieDes');
-        bodyNightTheme.style["background-color"] = "#020202";
-        SVGNightTheme.style["background-color"] = "#101010";
-        SVGNightTheme.style["box-shadow"] = "0px 0px 1000px #000000";
-        HeadingNightTheme.style["color"] = "#ffffff";
-        DescriptionNightTheme.style["color"] = "#404040";
-        Cookie.style['background-color'] = "#101010";
-        CookieHeading.style['color'] = "#f4f7f8";
-        CookieDes.style['color'] = "#797979";
+  document.oncontextmenu = new Function("return false;")
+  document.addEventListener('contextmenu', e => e.preventDefault());
+  document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+      alert("This function has been disabled to prevent you from stealing my code!");
+      return false;
     }
-  
-    document.addEventListener('visibilitychange', function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+      alert("This function has been disabled to prevent you from stealing my code!");
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+      alert("This function has been disabled to prevent you from stealing my code!");
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+      alert("This function has been disabled to prevent you from stealing my code!");
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+      alert("This function has been disabled to prevent you from stealing my code!");
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
+      alert("Just try being original for once!");
+      return false;
+    }
+  }
+
+  var dayMapper = {"0": "Sunday", "1": "Monday", "2": "Tuesday", "3":"Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday"}
+  var MonthMapper = {"1": "January", "2": "February", "3": "March", "4": "April", "5":"May", "6": "June", "7": "July", "8": "August", "9": "September", "10": "October", "11": "November", "12": "December"};
+  var today = new Date();
+  var timeInHour = today.getHours();
+  var timeInMinutes = today.getMinutes();
+  var Day = today.getDay();
+
+  if (timeInHour >= 19 || timeInHour < 5) {
+      var HREF = document.getElementById('href');
+      HREF.style["color"] = "#888888";
+      // var bodyNightTheme = document.getElementById('bodyNightTheme');
+      // var Showcase = document.getElementById('Showcase');
+      // var News = document.getElementById('News');
+      // bodyNightTheme.style["background-color"] = "#0F0F13";
+      // Showcase.style["background-color"] = "#1e1e26";
+      // News.style["background-color"] = "#1e1e26";
+      // document.querySelector('meta[itemprop="image"]').setAttribute(content="");
+      document.documentElement.style.setProperty('--theme-responsive-color', 'rgba(255, 255, 255, 0.05)');
+      document.documentElement.style.setProperty('--theme-extra-color', '#16161c');
+      document.documentElement.style.setProperty('--theme-day-night-background', '#0F0F13');
+      document.documentElement.style.setProperty('--theme-day-night-foreground', '#ffffff');
+      document.documentElement.style.setProperty('--theme-day-night-substitute', '#333333');
+      document.documentElement.style.setProperty('--theme-day-night-filter', 'invert(100%) sepia(100%) saturate(0%) hue-rotate(126deg) brightness(105%) contrast(104%)');
+  }
+
+  document.addEventListener('visibilitychange', function(e) {
       var isPageActive = !document.hidden;
   
       if(!isPageActive){
-        blink();
+          blink();
       }else {
-        document.title = pageTitle;
-        clearInterval(blinkEvent);
+          document.title = pageTitle;
+          clearInterval(blinkEvent);
       }
-    });
-  
-    function blink(){
+  });
+
+  function blink(){
       blinkEvent = setInterval(function() {
-        if(document.title === attentionMessage1){
-          document.title = attentionMessage2;
-        }else {
-          document.title = attentionMessage1;
+          if(document.title === attentionMessage1){
+              document.title = attentionMessage2;
+          }else {
+              document.title = attentionMessage1;
         }
       }, 100);
-    }
+  }
 };
 
-async function cookieAccepted() {
-  var Cookie = document.getElementById('Footer');
-  var Footer = document.getElementById('Footer');
-  Cookie.style['-webkit-animation'] = "slide-out-bottom 0.4s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
-  Cookie.style['animation'] = "slide-out-bottom 0.4s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
-  await new Promise(r => setTimeout(r, 500));
-  Footer.style['display'] = "none";
-}
+// async function blurNavBar() {
+//     var NavBar = document.getElementById('NavBar');
+//     NavBar.style["backdrop-filter"] = "blur(75px)";
+//     NavBar.style["height"] = "calc(var(--theme-responsive-size)*1.75)";
+//     await new Promise(r => setTimeout(r, 500));
+//     NavBar.style["backdrop-filter"] = "blur(0px)";
+//     //NavBar.style["height"] = "alc(var(--theme-responsive-size)*2.25)";
+// }
